285006c8586d56b4907ec7ad820a32c63ecc566e
yangfuzhang0720@126.com <yangfuzhang0720@126.com>
2023-11-15 22:32:35 +0800

    [feat] 增加二级页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });

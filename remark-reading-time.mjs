8fb5e386dda9a1e7600598637caac84455043920
yangfuzhang0720@126.com <yangfuzhang0720@126.com>
2023-12-01 11:13:18 +0800

    [feat] 阅读时间修改

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -6,6 +6,6 @@ export function remarkReadingTime() {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
 
-    data.astro.frontmatter.minutesRead = readingTime.text;
+    data.astro.frontmatter.minutesRead = `${Math.ceil(readingTime.minutes)}min`;
   };
 }

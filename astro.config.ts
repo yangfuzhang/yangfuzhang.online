55cc1600d484074ed73d3766cb33cb0e4183002d
yangfuzhang <yangfuzhang0720@126.com>
2025-10-08 17:53:35 +0800

    [feat] 博客改版

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,24 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import vue from "@astrojs/vue";
-import UnoCSS from "unocss/astro";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: node({
     mode: "standalone",
   }),
+
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  integrations: [
-    vue(),
-    UnoCSS({
-      // injectReset: true,
-    }),
-  ],
+
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });

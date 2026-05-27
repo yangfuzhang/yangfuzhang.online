bd1145224c425ea265d6c8e6567bd0f0d617d7c3
yangfuzhang <yangfuzhang0720@126.com>
2026-05-27 19:36:53 +0800

    [feat] 解决ts报错

--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -1,7 +1,7 @@
 import { HeroUIProvider } from "@heroui/react";
 import { ToastProvider as HeroToastProvider } from "@heroui/toast";
 
-export function ToastProvider({ children }) {
+export function ToastProvider({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
       <HeroToastProvider />
